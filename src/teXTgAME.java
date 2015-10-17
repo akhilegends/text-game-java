e387ba9bbf1b67f6f11b47881aca6176ea53610f
Akhil Nair <akhisuperkid@gmail.com>
2015-10-17 12:32:15 -0500

    Cleaning
    Do Something

--- src/teXTgAME.java
+++ src/teXTgAME.java
@@ -2,26 +2,25 @@
 public class teXTgAME {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int getInt = IO.getInt("Enter:\n 8. For moving Forward\n 2. "
 				+ "For moving backwards\n "
-				+ "4. ForGoing left\n 6. For Going right");
+				+ "4. For Going left\n 6. For Going right");
 		String movementMethod = movement(getInt);
 		output(getInt, movementMethod);
 	}
 	public static String movement(double g){
 		if (g == 8)
 			return "You move Forward";
-		
+
 		else if (g == 6)
 			return "You move to the right";
-		
+
 		else if (g == 4)
 			return "You move to the left"; 
-		
+
 		else if (g == 2)
 			return "You move backwards";
-		
+
 		else
 			return "Please Enter the Valid Numbers";
 
@@ -29,5 +28,5 @@ public class teXTgAME {
 	public static void output(double g, String m) {
 		System.out.print(m);
 	}
-	
+
 }
